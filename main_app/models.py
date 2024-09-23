7eea683b78162cc0b7ec0e041121333da3663f74
Jordan Janszen <jordanjanszen@hotmail.com>
2024-09-23 14:11:54 +1000

    Created Event model and added superuser

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.TextField(max_length=250)
+    category = models.CharField(max_length=100)
+    time = models.DateTimeField(default=timezone.now)
+    comment = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 
+from .models import Event
+
+events = [
+    {'name': 'Morning Run', 'location': 'Sydney', 'category': 'Exercise', 'time': '10am', 'comment': 'Felt fast as f***!'},
+    {'name': 'Brunch', 'location': 'Sydney', 'category': 'Social', 'time': '11am', 'comment': 'I ate so much food...'}
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def events_index(request):
+    events = Event.objects.all()
+    return render(request, 'events/index.html', {
+        'events' : events
+    })
+
